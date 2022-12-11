217d42bff41f7ec3d0f76f5379c23bf8a02e3ddf
namth <namth.pass@gmail.com>
2022-12-11 20:22:41 +0700

    MOMO ipn update

--- header-topbar.php
+++ header-topbar.php
@@ -16,6 +16,13 @@ if (is_user_logged_in()) {
             <span>Xin chào, <b><?php echo $user->data->display_name . " (" . $login_amount . ")"; ?></b></span>
             <img src="<?php echo $link_avatar; ?>" alt="" data-mui-toggle="dropdown">
             <ul class="mui-dropdown__menu">
+                <?php 
+                    wp_nav_menu(array(
+                        'menu'      => 5,
+                        'container' => '',
+                        'items_wrap' => '%3$s'
+                    ));
+                ?>
                 <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-home">
                     <a href="<?php echo get_author_posts_url($user->ID); ?>"><i class="fa fa-id-card-o" aria-hidden="true"></i> Cài đặt tài khoản</a>
                 </li>
--- inc/payment/momo_endpoint.php
+++ inc/payment/momo_endpoint.php
@@ -12,7 +12,42 @@ function momo_endpoint(WP_REST_Request $request) {
     $json_result = json_decode($request->get_body());
 
     $extraData = json_decode(base64_decode($json_result->extraData));
-    update_field('field_6394b76112cf9', $json_result, $extraData->inova_orderid);
+    $inova_orderid = $extraData->inova_orderid;
+    $activate = get_field('activate', $inova_orderid);
+    update_field('field_6394b76112cf9', $json_result, $inova_orderid);
+
+    $signature = $json_result->signature;
+    //before sign HMAC SHA256 signature
+    $rawHash =  
+        "accessKey=" . MOMO_ACCESS_KEY . 
+        "&amount=" . $json_result->amount .
+        "&extraData=" . $json_result->extraData .
+        "&message=" . $json_result->message .
+        "&orderId=" . $json_result->orderId .
+        "&orderInfo=" . $json_result->orderInfo .
+        "&orderType=" . $json_result->orderType .
+        "&partnerCode=" . $json_result->partnerCode .
+        "&payType=" . $json_result->payType .
+        "&requestId=" . $json_result->requestId .
+        "&responseTime=" . $json_result->responseTime .
+        "&resultCode=" . $json_result->resultCode .
+        "&transId=" . $json_result->transId;
+
+    $check_signature = hash_hmac("sha256", $rawHash, MOMO_SECRET_KEY);
+
+    # Nếu chưa active và xác nhận chữ ký đúng thì cập nhật trạng thái đơn hàng
+    if ( !$activate && ($json_result->resultCode == 0) && ($signature == $check_signature)) {
+        # chuyển trạng thái đơn hàng
+        update_field('field_62eb93b78ca79', "Đã thanh toán", $inova_orderid);
+        update_field('field_636c85b89d08e', 'Thanh toán qua ví MOMO', $inova_orderid); #phương thức thanh toán
+        # kích hoạt gói
+        $active_done = activation_package($inova_orderid);
+
+        if ($active_done) {
+            # Đánh dấu đơn hàng đã được active 
+            update_field('field_636df1ce10556', true, $inova_orderid);
+        }
+    }
 
     return new WP_REST_Response(null, 204);
 }
@@ -43,17 +78,20 @@ function momo_check_order($data = []) {
 
     # Nếu không có lỗi gì và xác nhận được chữ ký hợp lệ thì kích hoạt gói.
     if ( ($resultCode == 0) && ($signature == $check_signature)) {
-        # chuyển trạng thái đơn hàng
-        update_field('field_62eb93b78ca79', "Đã thanh toán", $inova_orderid);
-        update_field('field_636c85b89d08e', 'Thanh toán qua ví MOMO', $inova_orderid); #phương thức thanh toán
-        # kích hoạt gói
-        $active_done = activation_package($inova_orderid);
-
-        if ($active_done) {
-            # Đánh dấu đơn hàng đã được active 
-            update_field('field_636df1ce10556', true, $inova_orderid);
+        $activate = get_field('activate', $inova_orderid);
+        if (!$activate) {
+            # chuyển trạng thái đơn hàng
+            update_field('field_62eb93b78ca79', "Đã thanh toán", $inova_orderid);
+            update_field('field_636c85b89d08e', 'Thanh toán qua ví MOMO', $inova_orderid); #phương thức thanh toán
+            # kích hoạt gói
+            $active_done = activation_package($inova_orderid);
+    
+            if ($active_done) {
+                # Đánh dấu đơn hàng đã được active 
+                update_field('field_636df1ce10556', true, $inova_orderid);
+            }
         }
-        # trả về thông báo
+        # trả về kết quả kích hoạt thành công
         return true;
     } else {
         return false;
