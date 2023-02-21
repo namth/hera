58ec4c98e358fdc8a897c71fa65529a95632c325
namth <namth.pass@gmail.com>
2023-02-21 09:15:08 +0700

    remove secret file

--- inc/general/function.php
+++ inc/general/function.php
@@ -4,5 +4,5 @@ require_once( $general_dir . '/custom_post.php');
 require_once( $general_dir . '/custom_field.php');
 
 /* Xử lý hàm liên quan tới thanh toán */
-require_once( $general_dir . '/secret.php');
+// require_once( $general_dir . '/secret.php');
 require_once( $general_dir . '/order.php');
