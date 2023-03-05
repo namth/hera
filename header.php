23afa90145e539608a5facb6dc4552fcd8ec38e0
namth <namth.pass@gmail.com>
2023-03-05 16:15:17 +0700

    update header

--- header.php
+++ header.php
@@ -3,8 +3,10 @@
 
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="<?php bloginfo('name'); ?>" name="description">
     <title><?php bloginfo('name'); ?> &raquo; <?php is_front_page() ? bloginfo('description') : wp_title(''); ?></title>
-    <meta content="Thiệp cưới online HERA" name="description">
 
     <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon.png" type="image/x-icon"/>
     <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon.png" type="image/x-icon"/>
