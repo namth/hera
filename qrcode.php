6ae5c13bb4c68f26b358fe55e24445dbddbad8d7
namth <namth.pass@gmail.com>
2023-11-13 17:12:03 +0700

    fix login zalo

--- qrcode.php
+++ qrcode.php
@@ -2,7 +2,7 @@
 /* 
 * Template name: QR Code generate
 */
-require_once get_template_directory() . '/vendor/autoload.php';
+/* require_once get_template_directory() . '/vendor/autoload.php';
 
 use Endroid\QrCode\Builder\Builder;
 use Endroid\QrCode\Encoding\Encoding;
@@ -11,7 +11,9 @@ use Endroid\QrCode\Label\Alignment\LabelAlignmentCenter;
 use Endroid\QrCode\Label\Font\NotoSans;
 use Endroid\QrCode\Logo\Logo;
 use Endroid\QrCode\Color\Color;
-use Endroid\QrCode\RoundBlockSizeMode\RoundBlockSizeModeMargin;
+use Endroid\QrCode\RoundBlockSizeMode;
+use Endroid\QrCode\RoundBlockSizeMode\RoundBlockSizeModeNone;
+use Endroid\QrCode\RoundBlockSizeMode\RoundBlockSizeModeShrink;
 use Endroid\QrCode\Writer\PngWriter;
 
 $result = Builder::create()
@@ -20,16 +22,64 @@ $result = Builder::create()
     ->data('http://localhost/hera/qrcode/')
     ->encoding(new Encoding('UTF-8'))
     // ->errorCorrectionLevel(new ErrorCorrectionLevelHigh())
-    // ->size(300)
+    ->size(300)
     ->margin(10)
-    ->roundBlockSizeMode(new RoundBlockSizeModeMargin())
+    ->roundBlockSizeMode(new RoundBlockSizeModeNone())
     // ->logoPath('http://localhost/hera/wp-content/uploads/2023/07/Fuzzy_LeeWedding-logo.png')
-    ->logoPath('http://localhost/hera/wp-content/uploads/2023/07/Leewedding.png')
+    ->logoPath('https://assets-prod.sumo.prod.webservices.mozgcp.net/media/uploads/images/2019-10-22-22-36-41-1c7095.png')
     ->logoResizeToWidth(82)
     ->logoPunchoutBackground(true)
     ->validateResult(false)
     ->build();
-
+ */
 // print_r($result);
 ?>
-<img src="<?php echo $result->getDataUri(); ?>" alt="QR Code">
+<!-- <img src="<?php //echo $result->getDataUri(); ?>" alt="QR Code"> -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>QR Code Styling</title>
+    <script type="text/javascript" src="https://unpkg.com/qr-code-styling@1.5.0/lib/qr-code-styling.js"></script>
+</head>
+<body>
+<div id="canvas"></div>
+<script type="text/javascript">
+
+    let dotColor = "#916d45";
+    let bgColor = "#916d4500";
+    let logo = "";
+    let data = "https://www.facebook.com/";
+
+    const qrCode = new QRCodeStyling({
+        width: 300,
+        height: 300,
+        type: "svg",
+        data: data,
+        image: logo,
+        dotsOptions: {
+            color: dotColor,
+            type: "dots"
+        },
+        cornersSquareOptions: {
+            color: dotColor,
+            type: "extra-rounded"
+        },
+        cornersDotOptions: {
+            type: "dot"
+        },
+        backgroundOptions: {
+            color: bgColor,
+        },
+        imageOptions: {
+            crossOrigin: "anonymous",
+            margin: 10
+        }
+    });
+
+    qrCode.append(document.getElementById("canvas"));
+    // qrCode.download({ name: "qr", extension: "svg" });
+</script>
+</body>
+</html>
